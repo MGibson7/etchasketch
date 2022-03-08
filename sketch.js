0908a8bd304247cbe24909cef95109a97ca1c718
MGibson7 <MGibsonASC@gmail.com>
2022-03-08 14:03:54 -0500

    edited file so it could do any number of boards up to 100X100

--- sketch.js
+++ sketch.js
@@ -8,7 +8,6 @@ function addContainerDiv(x){
         classNames = document.getElementById("container")
         classNames.append(new_div);
     }
-    console.log(container);
     
 
 
@@ -18,35 +17,28 @@ function addDiv(x, theClassName){
     for (i=0; i<x; i++){
         let new_div = document.createElement("div");
         new_div.classList.add(`gridItem`);
-        new_div.setAttribute('style', 'flex-basis: 100%; border: 10px solid green; padding: 10px;');
-        console.log(theClassName);
+        new_div.setAttribute('style', 'flex-basis: 100%; border: 5px solid black; padding: 10px;');
         let theClass = document.querySelector(`${theClassName}`);
-        console.log(theClass);
         theClass.append(new_div);
     }
-    console.log(container);
 
 
 };
 
 
-addContainerDiv(16);
-addDiv(16, ".grid0");
-addDiv(16, ".grid1");
-addDiv(16, ".grid2");
-addDiv(16, ".grid3");
-addDiv(16, ".grid4");
-addDiv(16, ".grid5");
-addDiv(16, ".grid6");
-addDiv(16, ".grid7");
-addDiv(16, ".grid8");
-addDiv(16, ".grid9");
-addDiv(16, ".grid10");
-addDiv(16, ".grid11");
-addDiv(16, ".grid12");
-addDiv(16, ".grid13");
-addDiv(16, ".grid14");
-addDiv(16, ".grid15");
+let user_choice = 24;
+
+function buildBoard(squares){
+    addContainerDiv(squares);
+    for (y = 0; y<squares; y++){
+        addDiv(squares, `.grid${y}`);
+        console.log(y);
+    }
+
+}
+
+buildBoard(user_choice);
+
 
 let grids = document.querySelectorAll(".gridItem");
 console.log(grids);
@@ -56,9 +48,7 @@ grids.forEach(element => {
         element.target.style.backgroundColor = "aqua";
 
     })
-    element.addEventListener("mouseout", function(element){
-        element.target.style.backgroundColor = "";
-    })
+
 })
 
 
