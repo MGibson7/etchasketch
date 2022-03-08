9227af7a934d6099c58757faf1646c43170c773d
MGibson7 <MGibsonASC@gmail.com>
2022-03-08 15:18:55 -0500

    added a clear button

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function addDiv(x, theClassName){
     for (i=0; i<x; i++){
         let new_div = document.createElement("div");
         new_div.classList.add(`gridItem`);
-        new_div.setAttribute('style', 'flex-basis: 100%; border: 5px solid black; padding: 10px;');
+        new_div.setAttribute('style', 'flex-basis: 100%; border: 5px solid black; padding: 5px;');
         let theClass = document.querySelector(`${theClassName}`);
         theClass.append(new_div);
     }
@@ -29,10 +29,12 @@ function addDiv(x, theClassName){
 let user_choice = 24;
 
 function buildBoard(squares){
+    if (squares > 100){
+        squares = 100;
+    }
     addContainerDiv(squares);
     for (y = 0; y<squares; y++){
         addDiv(squares, `.grid${y}`);
-        console.log(y);
     }
 
 }
@@ -41,7 +43,7 @@ buildBoard(user_choice);
 
 
 let grids = document.querySelectorAll(".gridItem");
-console.log(grids);
+
 
 grids.forEach(element => {
     element.addEventListener("mouseenter", function(element){
@@ -51,4 +53,14 @@ grids.forEach(element => {
 
 })
 
+let clear = document.getElementById("clearBoard");
 
+
+
+clear.addEventListener("click", function(){
+    grids.forEach(element => {
+        element.style.backgroundColor = "";
+
+    })
+})
+    
